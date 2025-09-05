84419221c4f1e1f1fa0ea199d3c01f859eb9e2da
vincent325 <vic325@lehigh.edu>
2025-09-04 20:51:46 -0400

    cyber theme

--- rickroll.js
+++ rickroll.js
@@ -1,7 +1,3 @@
 function rickRoll() {
-    for(let i = 0; i < 50; i++) {
-        setTimeout(() => {
-            window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", '_blank');
-        }, i * 300);
-    }
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
